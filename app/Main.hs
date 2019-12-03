4b80cd8b44a3dddcff971a817a32607cb2eecc97
jessrenteria <jessrenteria@jessrenteria.local>
2019-12-02 23:19:02 -0800

    Cop out and use ugly IO escape hatch for day2part2.
    I'll resolve this when I have time.

--- app/Main.hs
+++ app/Main.hs
@@ -1,15 +1,18 @@
 {-# LANGUAGE FlexibleContexts #-}
 module Main where
 
+import Control.Exception
 import Control.Monad.ST
 import Data.Array.IArray as IArray
 import Data.Array.MArray as MArray
 import Data.Array.ST as STArray
+import Data.Either
+import System.IO.Unsafe
 
 import Lib
 
 main :: IO ()
-main = putStrLn . show =<< solve "data/day2.txt" parseDay2 solveDay2
+main = putStrLn . show =<< solve "data/day2.txt" parseDay2 solveNext2
 
 solve :: (Show b) => String -> (String -> a) -> (a -> b) -> IO b
 solve filepath parser solver = do
@@ -54,10 +57,22 @@ handleBinaryOp op instrPtr arr = do
   MArray.writeArray arr zIdx (x `op` y)
   handleCode (instrPtr + 4) arr
 
-solveDay2 :: [Int] -> Int
-solveDay2 xs = runST $ do
+runIntcode :: [Int] -> (Int, Int) -> Int
+runIntcode xs (y, z) = runST $ do
   arr <- listToArray xs :: ST s (STArray.STUArray s Int Int)
-  MArray.writeArray arr 1 12
-  MArray.writeArray arr 2 2
+  MArray.writeArray arr 1 y
+  MArray.writeArray arr 2 z
   handleCode 0 arr
   MArray.readArray arr 0
+
+solveDay2 :: [Int] -> Int
+solveDay2 xs = runIntcode xs (12, 2)
+
+-- Disgusting and shameful but quick to implement.
+-- TODO: explore properly handling failure in runIntcode (with an Either?).
+solveNext2 :: [Int] -> (Int, Int)
+solveNext2 xs = fst $ head $ filter ((== 19690720) . snd) $ rights $ unsafePerformIO $ mapM run candidates
+  where
+    candidates = [ (y, z) | y <- [0..99], z <- [0..99] ]
+    run :: (Int, Int) -> IO (Either SomeException ((Int, Int), Int))
+    run candidate = try $ evaluate $ (candidate, runIntcode xs candidate)
